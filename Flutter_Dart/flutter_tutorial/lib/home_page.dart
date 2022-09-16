53eb3731020d2a547cc4d67a5aea1ef80dec08d5
dev-yuri-ferreira <yuri.ferreira.contato@outlook.com>
2022-09-16 10:46:38 -0300

    minor changes

--- Flutter_Dart/flutter_tutorial/lib/home_page.dart
+++ Flutter_Dart/flutter_tutorial/lib/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_tutorial/learn_flutter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Center(
