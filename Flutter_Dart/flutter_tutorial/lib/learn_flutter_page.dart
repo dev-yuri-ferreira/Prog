c869be9bd21bede3222395c8e990deb021f5e355
dev-yuri-ferreira <yuri.ferreira.contato@outlook.com>
2022-09-16 10:20:24 -0300

    finished tutorial

--- Flutter_Dart/flutter_tutorial/lib/learn_flutter_page.dart
+++ Flutter_Dart/flutter_tutorial/lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,71 +22,109 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
-      ),
-      body: Column(
-        children: [
-          Image.asset('images/neco-arc.gif'),
-          const SizedBox(
-            height: 10.0,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                'This is a text widget',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-          ElevatedButton(
+        actions: [
+          IconButton(
             onPressed: () {
-              debugPrint('Elevated Button');
+              debugPrint('Actions');
             },
-            child: const Text('Elevated Button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Outlined Button');
-            },
-            child: const Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('Text Button');
-            },
-            child: const Text('Text Button'),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint('This is the Row!');
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.indigo,
-                ),
-                Text(
-                  'Row Widget',
-                ),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.indigo,
-                ),
-              ],
+            icon: const Icon(
+              Icons.info_outline,
             ),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/neco-arc.gif'),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is a text widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                // ignore: deprecated_member_use
+                primary: isSwitch ? Colors.lime : Colors.deepPurple,
+              ),
+              onPressed: () {
+                debugPrint('Elevated Button');
+              },
+              child: const Text('Elevated Button'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined Button');
+              },
+              child: const Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Text Button');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('This is the Row!');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.indigo,
+                  ),
+                  Text(
+                    'Row Widget',
+                  ),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.indigo,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(
+                  () {
+                    isSwitch = newBool;
+                  },
+                );
+              },
+            ),
+            Checkbox(
+              value: isCheckbox,
+              onChanged: (bool? newBool) {
+                setState(
+                  () {
+                    isCheckbox = newBool;
+                  },
+                );
+              },
+            ),
+            Image.network(
+                'https://c.tenor.com/Hs0fUlHHGawAAAAi/neco-arc-lumina.gif'),
+          ],
+        ),
       ),
     );
   }
